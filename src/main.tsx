255d92e991375a56160596c3fe26ecec89a3cc13
Bushei <balbla92@gmail.com>
2022-10-15 02:04:19 +0200

    installed vite-plugin-eslint, changed imports paths to aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '@/App';
+
 import './index.css';
+import 'node_modules/css-reset-and-normalize/css/reset-and-normalize.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [tsconfigPaths(), eslint(), react()],
   server: {
     port: 2137,
   },
