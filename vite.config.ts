674c263ceb28d952b72ac09429e9e7db16a6dbd4
Buashei <balbla92@gmail.com>
2022-12-26 21:52:37 +0100

    plopfile - moved to TS, added vitest to project, project is docerized

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,6 +7,12 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), eslint(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // include: ['**/**/*.{test}.{ts,mts,cts,tsx}'],
+    setupFiles: './vitest.setup.ts',
+  },
   server: {
     watch: {
       usePolling: true,
