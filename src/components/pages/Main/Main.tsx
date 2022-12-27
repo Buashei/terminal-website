58b17acc92e0c3aff231ce37ab2f5e96871e26b7
Buashei <balbla92@gmail.com>
2022-12-28 00:43:49 +0100

    First implementation of Main component, main service implementation started

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -1,5 +1,72 @@
+import { useEffect, useState } from 'react';
+import { MainService } from './Main.service';
+// import type { KeyboardEvent } from 'react';
 import type { TMain } from './Main.types';
 
+const mainService = new MainService();
+
 export const Main: TMain = () => {
-  return <div></div>;
+  const [state, setState] = useState(mainService.state);
+
+  useEffect(() => {
+    mainService.subscribe(setState);
+
+    return () => mainService.unsubscribe(setState);
+  }, []);
+
+  // const handleEnterKey = (e: KeyboardEvent<HTMLInputElement>) => {
+  //   if (e.key === 'Enter') {
+  //     mainService.setTerminalOutput(state.terminalInput);
+  //     mainService.setTerminalInput('');
+  //   }
+  // };
+
+  return (
+    <>
+      <div className='terminalOutput'>
+        {state.terminalOutput.map((element, idx) => {
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={`element${idx}`} style={{ marginBottom: '0.2rem' }}>
+              <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '0.1rem' }}>
+                <div style={{ flexShrink: 1 }}>
+                  <span style={{ color: 'rgb(215, 153, 33)' }}>guest</span>
+                  <span style={{ color: 'rgb(168, 153, 132)' }}>@</span>
+                  <span style={{ color: 'rgb(152, 151, 26)' }}>buashei.codes</span>
+                  <span style={{ color: 'rgb(168, 153, 132)' }}>:$ ~</span>
+                </div>
+                <div style={{ flexGrow: 1, marginLeft: '0.5rem' }}>commandName</div>
+              </div>
+              <div className='typewriter'>{element}</div>
+            </div>
+          );
+        })}
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'row', width: '100vw' }}>
+        <label htmlFor='prompt' style={{ flexShrink: 1 }}>
+          <span style={{ color: 'rgb(215, 153, 33)' }}>guest</span>
+          <span style={{ color: 'rgb(168, 153, 132)' }}>@</span>
+          <span style={{ color: 'rgb(152, 151, 26)' }}>buashei.codes</span>
+          <span style={{ color: 'rgb(168, 153, 132)' }}>:$ ~</span>
+        </label>
+        <input
+          id='prompt'
+          type='text'
+          className='terminalInput'
+          onBlurCapture={(e) => e.target.focus()}
+          value={state.terminalInput}
+          onChange={(e) => mainService.setTerminalInput(e.target.value)}
+          onKeyDown={(e) => mainService.handleEnter(e)}
+          style={{
+            color: '#FFFFFF',
+            backgroundColor: 'transparent',
+            border: 'transparent',
+            outline: 'transparent',
+            flexGrow: 1,
+            marginLeft: '0.5rem',
+          }}
+        />
+      </div>
+    </>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import { Main } from './components/pages/Main/Main';
 
 import './index.css';
-import 'node_modules/css-reset-and-normalize/css/reset-and-normalize.min.css';
+import './normalize.css';
+// import 'node_modules/css-reset-and-normalize/css/reset-and-normalize.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
